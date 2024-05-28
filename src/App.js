03c0c5cc23315db52f8c3b8d6076f75a088f7818
Nurbolat Duisenbek <78546087+nurbolatkz@users.noreply.github.com>
2024-05-28 13:20:07 +0000

    change styles of project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import SubjectsDetail from './components/SubjectsDetail';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import SubjectsDetail from "./components/SubjectsDetail";
 
 function App() {
-  const [selectedContent, setSelectedContent] = useState('');
+  const [selectedContent, setSelectedContent] = useState("");
   const [isNavbarOpen, setIsNavbarOpen] = useState(false);
 
   const handleNavbarItemClick = (content) => {
@@ -17,14 +17,15 @@ function App() {
 
   return (
     <div>
-      <Navbar onNavbarItemClick={handleNavbarItemClick} isNavbarOpen={isNavbarOpen} toggleNavbar={toggleNavbar} />
-      <div className={`selected_content ${isNavbarOpen ? 'navbar-opened' : ''}`}>
-        {selectedContent}
-        <SubjectsDetail />
-      </div>
+      <Navbar
+        onNavbarItemClick={handleNavbarItemClick}
+        isNavbarOpen={isNavbarOpen}
+        toggleNavbar={toggleNavbar}
+      />
+
+      <SubjectsDetail />
     </div>
   );
 }
 
 export default App;
-
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,83 +1,27 @@
-import React from 'react';
-import '../style.css'; 
+import React from "react";
+import "../style.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function Navbar({ onNavbarItemClick, isNavbarOpen, toggleNavbar }) {
-    const handleItemClick = (content) => {
-      onNavbarItemClick(content);
-    };
-  
-    return (
-    <div id="nav-bar">
-         <input id="nav-toggle" type="checkbox" checked={isNavbarOpen} onChange={toggleNavbar} />
-      
-      <div id="nav-header">
-        <a id="nav-title" href="#" >
-          BirQadam
-        </a>
-        <label htmlFor="nav-toggle">
-          <span id="nav-toggle-burger"></span>
-        </label>
-        <hr />
-      </div>
-      <div id="nav-content">
-         <div className="nav-button" onClick={() => handleItemClick('Басты бет')}>
-            <i className="fas fa-palette"></i>
-            <span>Басты бет</span>
-        </div>
-        <div className="nav-button"  onClick={() => handleItemClick('Курстар')}>
-          <i className="fas fa-images"></i>
-          <span>Курстар</span>
-        </div>
-        <div className="nav-button"  onClick={() => handleItemClick('Тесттер')}>
-          <i className="fas fa-thumbtack"></i>
-          <span>Тесттер</span>
-        </div>
-        <hr />
-        <div className="nav-button">
-          <i className="fas fa-heart"></i>
-          <span>Жетістіктерім</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-chart-line"></i>
-          <span>Тапсырмалар</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-fire"></i>
-          <span>Материалдар</span>
-        </div>
-        <div className="nav-button">
-          <i className="fas fa-magic"></i>
-          <span>Жетекшілер</span>
-        </div>
-        <hr />
-        <div className="nav-button">
-          <i className="fas fa-gem"></i>
-          <span>Рейтинг</span>
-        </div>
-        <div id="nav-content-highlight"></div>
-      </div>
-      <input id="nav-footer-toggle" type="checkbox" />
-      <div id="nav-footer">
-        <div id="nav-footer-heading">
-          <div id="nav-footer-avatar">
-            <img src="https://gravatar.com/avatar/4474ca42d303761c2901fa819c4f2547" alt="avatar" />
-          </div>
-          <div id="nav-footer-titlebox">
-            <a id="nav-footer-title" href="https://codepen.io/uahnbu/pens/public">
-              uahnbu
-            </a>
-            <span id="nav-footer-subtitle">Admin</span>
-          </div>
-          <label htmlFor="nav-footer-toggle">
-            <i className="fas fa-caret-up"></i>
-          </label>
-        </div>
-        <div id="nav-footer-content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </div>
-      </div>
-    </div>
+function NavbarContainer({ onNavbarItemClick, isNavbarOpen, toggleNavbar }) {
+  const handleItemClick = (content) => {
+    onNavbarItemClick(content);
+  };
+
+  return (
+    <Navbar bg="primary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">BirQadam.kz</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Басты бет</Nav.Link>
+          <Nav.Link href="#features">Пәндер</Nav.Link>
+          <Nav.Link href="#pricing">Тесттер</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavbarContainer;
